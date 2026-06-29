d253fc671e8e78e979c7b2238e495b05758ad1ba
Fabse <fabianaloisschneider@icloud.com>
2026-06-29 13:23:01 +0200

    Übung 8.2 und Übung 8.4

---
+++ Uebung8_4.py
@@ -0,0 +1,11 @@
+import time
+
+while True:
+    print('\b/', end='', flush=True)
+    time.sleep(0.25)
+    print('\b-', end='', flush=True)
+    time.sleep(0.25)
+    print('\b\\', end='', flush=True)
+    time.sleep(0.25)
+    print('\b|', end='', flush=True)
+    time.sleep(0.25)
