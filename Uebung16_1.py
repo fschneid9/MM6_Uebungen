e9984b3e7cacad1d66b1c80ee04730a012fbf820
Fabse <fabianaloisschneider@icloud.com>
2026-07-09 13:07:59 +0200

    Übung 16.1

---
+++ Uebung16_1.py
@@ -0,0 +1,63 @@
+import subprocess
+from Bio.Seq import Seq
+from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
+import time
+import json
+
+infile='unknown.fas'    #Fasta-Datei mit Sequenz
+outfile='16_1.gff'      #gff-Datei, die von Prodega ausgegeben wird
+subprocess.run(['prodigal','-i',infile,'-f','gff','-o',outfile])
+
+gff=open(outfile, 'r')    #generierte gff-Datei öffnen
+fasta=open(infile, 'r')     #Fasta-Datei mit der gesamt-DNA-Sequenz wird geöffnet
+sequence=fasta.readlines()[1]   #als Sequenz wird die zweite Zeile der Datei ausgewählt. Die erste enthält ja den Header.
+
+dic={}  #leeres Ziel-Dictionary
+l=0
+
+for line in gff:
+    if line.startswith('#'):    #Header-Zeilen ignorieren
+        continue
+
+    cols=line.strip().split('\t')   #Tabelle anhand der Tabulatoren in Spalten teilen
+    if cols[0]!='unknown':  #wenn kein Genname bekannt ist, wird das Gen als geneX (X=fortlaufende Zahl) bezeichnet
+        id=cols[0]
+    else:
+        id=f'gene{l}'
+    
+    #Jetzt werden die einzelnen Informationen aus den Spalten gezogen
+    strand=cols[6]
+    start=int(cols[3])
+    stop=int(cols[4])
+    seq_len=stop-start+1    #+1, das auch die start-position schon ein nucleotid hat
+    score=float(cols[5])
+
+    #Sequenzen
+    if strand=='+':     #Wenn wir auf dem FW-Strand sind, entspricht die DNA-Sequenz dem Bereich zwischen Start und Stopp
+        dna_seq=Seq(sequence[start-1:stop]) #-1, da in die Positions-Zählweise in der Sequenz bei 1 beginnt, Python aber ab 0 zählt.
+    elif strand=='-':   #Wenn wir auf dem RV-Strand sind, entsprcht die DNA-Sequenz dem Reverse-Complement
+        dna_seq=Seq(sequence[start-1:stop]).reverse_complement()
+    aa_seq=dna_seq.translate()
+
+    dic.update({id:{    #Im Ziel-Dictionary wird zu jedem Gen ein eigenes Dictionary mit den Informationen erstellt
+        'start':start,
+        'stop':stop,
+        'length':seq_len,
+        'strand':strand,
+        'score':score,
+        'dna_sequence':dna_seq,
+        'protein_sequence':aa_seq
+    }
+    })
+
+
+    l+=1    #l wird für die fortlaufende Nummer bei der Gen-Benennung hochgezählt
+
+gff.close()   #dateien werden geschlossen
+fasta.close()
+
+#hier sieht man jetzt beispielhaft den Eintrag für das erste potentielle gen:
+print(dic['gene1'])
+
+
